b0df31f3445f172ef009c5951556e90149130646
Allan <allan.mercou@icloud.com>
2019-11-13 23:08:33 +0100

    Adds a few docs

--- src/OsuSharp/Entities/Replay.cs
+++ src/OsuSharp/Entities/Replay.cs
@@ -7,13 +7,13 @@ namespace OsuSharp
         internal Replay() { }
 
         /// <summary>
-        ///     The type of encoding used to encode <see cref="Content"/>
+        ///     The type of encoding used to encode <see cref="Content"/>. 
         /// </summary>
         [JsonProperty("encoding")]
         public string Encoding { get; internal set; }
 
         /// <summary>
-        ///     Content of the replay.
+        ///     Content of the replay. Use the <see cref="ReplayFile"/> to play with the replay or create a osr file from it.
         /// </summary>
         [JsonProperty("content")]
         public string Content { get; internal set; }
